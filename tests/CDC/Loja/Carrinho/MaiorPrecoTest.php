9891a204db9a793d26654ebf78522711a2763e3d
Thomas Moraes <tho.vini7@gmail.com>
2022-09-09 14:15:39 -0300

    Qualidade nos testes

--- tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
+++ tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
@@ -7,36 +7,50 @@ use PHPUnit\Framework\TestCase;
 
 class MaiorPrecoTest extends TestCase
 {
+    private $carrinho;
+
+    /**
+     * Método de inicialização da classe de testes
+     * Evita código duplicado e instâncias desnecessárias
+     * 
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->carrinho = new CarrinhoDeCompras();
+        parent::setUp();
+    }
+
     public function testDeveRetornarZeroSeCarrinhoVazio()
     {
-        $carrinho = new CarrinhoDeCompras();
+        // $carrinho = new CarrinhoDeCompras();
 
         $algoritmo = new MaiorPreco();
-        $valor = $algoritmo->encontra($carrinho);
+        $valor = $algoritmo->encontra($this->carrinho);
 
         $this->assertEquals(0, $valor);
     }
 
     public function testDeveRetornarValorDoItemSeCarrinhoCom1Elemento()
     {
-        $carrinho = new CarrinhoDeCompras();
-        $carrinho->adiciona(new Produto("Geladeira", 1, 900.00));
+        // $carrinho = new CarrinhoDeCompras();
+        $this->carrinho->adiciona(new Produto("Geladeira", 1, 900.00));
 
         $algoritmo = new MaiorPreco();
-        $valor = $algoritmo->encontra($carrinho);
+        $valor = $algoritmo->encontra($this->carrinho);
 
         $this->assertEquals(900, $valor);
     }
 
     public function testDeveRetornarMaiorValorCarrinhoComMuitosItens()
     {
-        $carrinho = new CarrinhoDeCompras();
-        $carrinho->adiciona(new Produto("Geladeira", 1, 900));
-        $carrinho->adiciona(new Produto("Fogão", 1, 1500));
-        $carrinho->adiciona(new Produto("Máquina de lavar", 1, 750));
+        // $carrinho = new CarrinhoDeCompras();
+        $this->carrinho->adiciona(new Produto("Geladeira", 1, 900));
+        $this->carrinho->adiciona(new Produto("Fogão", 1, 1500));
+        $this->carrinho->adiciona(new Produto("Máquina de lavar", 1, 750));
 
         $algoritmo = new MaiorPreco();
-        $valor = $algoritmo->encontra($carrinho);
+        $valor = $algoritmo->encontra($this->carrinho);
 
         $this->assertEquals(1500, $valor);
     }
