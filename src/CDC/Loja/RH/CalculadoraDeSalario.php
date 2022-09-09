7e571751305bc171dd5c6ee214f42addaa1bf401
Thomas Moraes <tho.vini7@gmail.com>
2022-09-09 18:37:04 -0300

    TDD e Coesao

--- src/CDC/Loja/RH/CalculadoraDeSalario.php
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -14,31 +14,4 @@ class CalculadoraDeSalario
 
         return $cargo->getRegra()->calcula($funcionario);
     }
-
-    // public function calculaSalario(Funcionario $funcionario)
-    // {
-    //     if($funcionario->getCargo() == TabelaCargos::DESENVOLVEDOR) {
-    //         return $this->dezOuVintePorCentoDeDesconto($funcionario);
-    //     } elseif($funcionario->getCargo() == TabelaCargos::DBA) {
-    //         return $this->quinzeOuVinteECincoPorCentoDeDesconto($funcionario);
-    //     }
-    //     throw new Exception("Tipo de funcionário inválido", 1);
-        
-    // }
-
-    // private function dezOuVintePorCentoDeDesconto(Funcionario $funcionario)
-    // {
-    //     if($funcionario->getSalario() > 3000) {
-    //         return $funcionario->getSalario() * 0.8;
-    //     }
-    //     return $funcionario->getSalario() * 0.9;
-    // }
-
-    // private function quinzeOuVinteECincoPorCentoDeDesconto($funcionario)
-    // {
-    //     if($funcionario->getSalario() < 2500) {
-    //         return $funcionario->getSalario() * 0.85;
-    //     }
-    //     return $funcionario->getSalario() * 0.75;
-    // }
 }
